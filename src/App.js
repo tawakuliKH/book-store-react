41002103f3820702c9b6d2d7d9343688ee920dfc
tawakuliKH <tawakuli456@gmail.com>
2023-11-03 17:19:51 +0330

    Configure react routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Book Store</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
