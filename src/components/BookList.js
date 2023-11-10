558749d3762e9bf7d42bbe3fc186581a5e4cba6e
tawakuliKH <tawakuli456@gmail.com>
2023-11-11 01:32:38 +0330

    Configure API connections with redux toolkit

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,30 @@
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/bookSlice';
+import { removeBook, getBooks } from '../redux/books/actions';
 
 const BookList = ({
   // eslint-disable-next-line react/prop-types
-  id, title, author,
+  id, title, author, category,
 }) => {
   const dispatch = useDispatch();
+  const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+
+  const handleRemove = () => {
+    dispatch(removeBook({ URL, id }))
+      .then(() => {
+        dispatch(getBooks(URL));
+      });
+  };
   return (
-    <div className="book-list" id={id}>
+    <div className="book-list" id={id} key={id}>
       <div className="book">
         <div className="bookTitle">
           <div className="action"> Action</div>
           <h3 className="title">{title}</h3>
           <h4 className="author">{author}</h4>
+          <h4 className="author">{category}</h4>
           <div className="comments-action">
             <button type="button" className="comment">Comment</button>
-            <button type="button" className="remove" onClick={() => { dispatch(removeBook(id)); }}>Remove</button>
+            <button type="button" className="remove" onClick={() => { handleRemove(id); }}>Remove</button>
             <button type="button" className="edit">Edit</button>
           </div>
         </div>
