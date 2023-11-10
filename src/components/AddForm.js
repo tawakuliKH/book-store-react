0faccc4ded0ad6c3cf363dd674dbad57f0302d87
tawakuliKH <tawakuli456@gmail.com>
2023-11-10 18:04:02 +0330

    Configure Remove book and Add Book reducers

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,13 +1,40 @@
-const AddForm = () => (
-  <div className="book-form">
-    <h3 className="add-book">Add New Book</h3>
-    <input type="text" placeholder="Book Name" />
-    <select name="cars" id="cars">
-      <option value="book1">Book 1</option>
-      <option value="book2">Book 2</option>
-    </select>
-    <button className="add-button" type="button">Add Book</button>
-  </div>
+/* eslint-disable no-unused-vars */
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addBook } from '../redux/books/bookSlice';
 
-);
+const AddForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const newBook = {
+    item_id: 'item4',
+    title,
+    author: '',
+    category,
+  };
+
+  const submitForm = () => {
+    dispatch(addBook(newBook));
+    setTitle('');
+  };
+  return (
+    <div className="book-form">
+      <h3 className="add-book">Add New Book</h3>
+      <input type="text" value={title} placeholder="Book Name" onChange={(e) => setTitle(e.target.value)} />
+      <select name="cars" id="cars" onChange={(e) => setCategory(e.target.value)}>
+        <option value="book1">Book 1</option>
+        <option value="book2">Book 2</option>
+      </select>
+      <button
+        className="add-button"
+        type="button"
+        onClick={submitForm}
+      >
+        Add Book
+      </button>
+    </div>
+  );
+};
+
 export default AddForm;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,44 +1,32 @@
-import AddForm from './AddForm';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/bookSlice';
 
-const Book = () => (
-  <div className="book-list">
-    <div className="book">
-      <div className="bookTitle">
-        <div className="action"> Action</div>
-        <h3 className="title">Book 1</h3>
-        <h4 className="author">John John</h4>
-        <div className="comments-action">
-          <button type="button" className="comment">Comment</button>
-          <button type="button" className="remove">Remove</button>
-          <button type="button" className="edit">Edit</button>
+const BookList = ({
+  // eslint-disable-next-line react/prop-types
+  id, title, author,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="book-list" id={id}>
+      <div className="book">
+        <div className="bookTitle">
+          <div className="action"> Action</div>
+          <h3 className="title">{title}</h3>
+          <h4 className="author">{author}</h4>
+          <div className="comments-action">
+            <button type="button" className="comment">Comment</button>
+            <button type="button" className="remove" onClick={() => { dispatch(removeBook(id)); }}>Remove</button>
+            <button type="button" className="edit">Edit</button>
+          </div>
+        </div>
+        <div className="percentage"> 64% </div>
+        <div className="book-info">
+          <div className="chapter">Chapter</div>
+          <div className="book-chapter">Chapter 17</div>
+          <button type="button" className="update">Update Progress</button>
         </div>
       </div>
-      <div className="percentage"> 64% </div>
-      <div className="book-info">
-        <div className="chapter">Chapter</div>
-        <div className="book-chapter">Chapter 17</div>
-        <button type="button" className="update">Update Progress</button>
-      </div>
     </div>
-    <div className="book">
-      <div className="bookTitle">
-        <div className="action"> Action</div>
-        <h3 className="title">Book 2</h3>
-        <h4 className="author">John John</h4>
-        <div className="comments-action">
-          <button type="button" className="comment">Comment</button>
-          <button type="button" className="remove">Remove</button>
-          <button type="button" className="edit">Edit</button>
-        </div>
-      </div>
-      <div className="percentage"> 64% </div>
-      <div className="book-info">
-        <div className="chapter">Chapter</div>
-        <div className="book-chapter">Chapter 17</div>
-        <button type="button" className="update">Update Progress</button>
-      </div>
-    </div>
-    <AddForm />
-  </div>
-);
-export default Book;
+  );
+};
+export default BookList;
