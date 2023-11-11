1ea774c8a55fec98f568b2e322865fb6d87d5307
tawakuliKH <tawakuli456@gmail.com>
2023-11-11 21:29:26 +0330

    Add styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PersonIcon from '@mui/icons-material/Person';
 
 const Navbar = () => (
   <div className="navbar">
-    <div className="title">
-      <h3>Book Store</h3>
-    </div>
     <div className="nav-links">
-      <Link to="/" className="nav-item nav-links-item">Books</Link>
-      <Link to="/Author" className="nav-item nav-links-item">Author</Link>
+      <h3 className="book-CMS">Book Store</h3>
+      <Link to="/" className="books">BOOKS</Link>
+      <Link to="/Author" className="categories  ">CATEGORIES</Link>
     </div>
+    <button type="button" className="iconButton">
+      <PersonIcon />
+    </button>
   </div>
 );
 export default Navbar;
